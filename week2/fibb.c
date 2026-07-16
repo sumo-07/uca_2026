46a2c8d2a0b584b381b4358055a478f9f8378f5f
Ujjawal Singh Bisht <sumo.cs07@gmail.com>
2026-07-16 20:53:31 +0530

    Week2 ques1

---
+++ week2/fibb.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int brute_force(int n) {
+    int fibs[100];
+    int count = 0;
+    int a = 1, b = 1;
+    while (a <= n) {
+        fibs[count++] = a;
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+    int sum = 0;
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 == 0) {
+            for (int j = 0; j < count; j++) {
+                if (fibs[j] == i) {
+                    sum += i;
+                    break;
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+int optimized(int n) {
+    int sum = 0;
+    int a = 2, b = 8;
+    if (n >= 2) {
+        sum += a;
+    }
+    while (b <= n) {
+        sum += b;
+        int next = 4 * b + a;
+        a = b;
+        b = next;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+    if (scanf("%d", &n) != 1) {
+        return 1;
+    }
+    int brute_res = brute_force(n);
+    int opt_res = optimized(n);
+    printf("%d\n", brute_res);
+    printf("%d\n", opt_res);
+    return 0;
+}
